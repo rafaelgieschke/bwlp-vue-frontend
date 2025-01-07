a51582b61de1f88abc24ceeda46064442822dcf2
Julien Rutscho <khodokpro@gmail.com>
2025-01-07 15:30:44 +0100

    fix modal

--- src/components/DetailModalComponent.vue
+++ src/components/DetailModalComponent.vue
@@ -51,35 +51,7 @@ defineEmits(['close']);
   background-color: rgba(0, 0, 0, 0.5);
 }
 
-.modal-content {
-  /* display: flex;
-  flex-direction: column;
-  width: 90%;
-  max-width: 500px;
-  max-height: 90vh;
-  padding: 1rem;
-  border-radius: 8px; */
-}
-
-.modal-header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 1rem;
-}
-
-.close-button {
-  padding: 0.1rem 0.5rem;
-}
-
 .modal-body {
-  margin-bottom: 1rem;
   overflow-y: auto;
-}
-
-.modal-footer {
-  display: flex;
-  justify-content: flex-end;
-  gap: 0.5rem;
 }
 </style>
