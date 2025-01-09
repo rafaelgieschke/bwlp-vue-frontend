3a3a7acb86e855b3c91cb8ddc8bf49a7605b3f52
Julien Rutscho <khodokpro@gmail.com>
2025-01-09 12:34:06 +0100

    extract header

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <template>
-  <header>
-    <nav>
-      <button class="circle transparent">
-        <i>menu</i>
-      </button>
-      <h5 class="max center-align">Baden-Württemberg Lehrpool</h5>
-      <button class="circle transparent">
-        <img class="responsive" src="@/assets/img/leopardskin.jpg" />
-      </button>
-      <button v-if="authStore.authToken" @click="logout">Logout</button>
-    </nav>
-  </header>
+  <HeaderComponent />
 
   <Navigation />
 
-  <main class="example-layout-main">
+  <main class="responsive">
     <router-view></router-view>
   </main>
 </template>
 
 <script setup>
-import {useRouter} from 'vue-router';
-import {useAuthStore} from '@/stores/auth-store';
 import Navigation from '@/components/Navigation.vue';
-
-const router = useRouter();
-const authStore = useAuthStore();
-
-const logout = () => {
-  authStore.clearToken();
-  router.push('/login');
-};
+import HeaderComponent from '@/components/HeaderComponent.vue';
 </script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <nav>
+      <button class="circle transparent">
+        <i>menu</i>
+      </button>
+      <h5 class="max center-align">Baden-Württemberg Lehrpool</h5>
+      <button class="">
+        Menu
+        <i>arrow_drop_down</i>
+        <menu>
+          <a>Profile</a>
+          <a v-if="authStore.authToken" href="#" @click.prevent="logout">
+            Logout
+          </a>
+        </menu>
+      </button>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import {useRouter} from 'vue-router';
+import {useAuthStore} from '@/stores/auth-store';
+
+const router = useRouter();
+const authStore = useAuthStore();
+
+const logout = () => {
+  authStore.clearToken();
+  router.push('/login');
+};
+</script>
