5d97b081fa5d01b2e74fb2d7eda16942c182b271
Julien Rutscho <khodokpro@gmail.com>
2025-01-29 13:57:27 +0100

    Netzlaufwerke thing

--- src/components/dialog/LectureTabs/LectureNetworkDrivesTab.vue
+++ src/components/dialog/LectureTabs/LectureNetworkDrivesTab.vue
@@ -1,10 +1,35 @@
 <template>
-  <EmptyTest text="Not yet implemented" />
+  <p>
+    Hier können Sie Netzlaufwerke angeben, die automatisch beim Start der
+    Veranstaltung eingebunden werden sollen. Der Platzhalter
+    <code>%loginuser%</code> wird im Pfad durch den Loginnamen des Nutzers
+    ersetzt.
+  </p>
+  <div v-if="lecture.networkShares" class="auto-height surface scroll">
+    <table class="stripes">
+      <thead class="fixed">
+        <tr>
+          <th>Name</th>
+          <th>Pfad</th>
+          <th>Laufwerk</th>
+          <th>Authentifizierung</th>
+          <th>Username</th>
+          <th>Passwort</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="networkShare in lecture.networkShares" :key="user_info">
+          <td v-for="element in networkShare">
+            <!-- No idea if that works as there seems to be no data? -->
+            {{ element }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
-import EmptyTest from '@/components/EmptyTest.vue';
-
 defineProps({
   lecture: {
     type: Object,
