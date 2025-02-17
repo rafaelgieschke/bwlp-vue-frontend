c1dc5aebc7d2208b42a2fc9688a1386fd4420917
Julien Rutscho <khodokpro@gmail.com>
2025-02-17 15:51:54 +0100

    SeparatingLabel uses proper TypeScript Removed useless props

--- src/components/navigation/SeparatingLabel.vue
+++ src/components/navigation/SeparatingLabel.vue
@@ -1,31 +1,19 @@
 <template>
-  <hr
-    v-if="separator"
-    class="small"
-    :class="(separatorSize, onlyMobileClass)"
-  />
+  <hr v-if="separator" class="small" :class="onlyMobileClass" />
 
   <!-- We use a Label despite it being a weird choice because this is how Beer CSS does it for some reason ¯\_(ツ)_/¯ -->
   <label :class="onlyMobileClass">{{ text }}</label>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  text: {
-    type: String,
-    required: true,
-  },
-  separator: {
-    type: Boolean,
-    default: true,
-  },
-  separatorSize: {
-    type: String,
-    default: 'small',
-  },
-  onlyMobileClass: {
-    type: String,
-    default: '',
-  },
+interface SeparatingLabelProps {
+  text: string;
+  separator?: boolean;
+  onlyMobileClass?: string;
+}
+
+withDefaults(defineProps<SeparatingLabelProps>(), {
+  separator: true,
+  onlyMobileClass: '',
 });
 </script>
