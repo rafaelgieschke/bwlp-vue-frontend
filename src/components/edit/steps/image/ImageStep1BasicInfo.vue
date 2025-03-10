b16c9a60f42e50bfcbc94723ab30f5b7c06ce1a6
Julien Rutscho <khodokpro@gmail.com>
2025-03-10 12:19:54 +0100

    Remove the Tags code as it's not implemented in the backend

--- src/components/edit/steps/image/ImageStep1BasicInfo.vue
+++ src/components/edit/steps/image/ImageStep1BasicInfo.vue
@@ -29,28 +29,6 @@
     </div>
 
     <SwitchTitle title="Is template" v-model="modelValue.isTemplate" />
-
-    <!-- <div class="field border">
-      <label>Tags</label>
-      <div class="tag-container">
-        <div
-          v-for="(tag, index) in modelValue.tags"
-          :key="index"
-          class="tag secondary"
-        >
-          {{ tag }}
-          <button type="button" @click="removeTag(index)">×</button>
-        </div>
-        <div class="add-tag">
-          <input
-            v-model="newTag"
-            @keyup.enter="addTag"
-            placeholder="Add a tag"
-          />
-          <button type="button" @click="addTag">+</button>
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
 
@@ -77,61 +55,4 @@ const osList = ref([]);
 onMounted(async () => {
   osList.value = await fetchOperatingSystems();
 });
-
-// const newTag = ref('');
-
-// const addTag = () => {
-//   if (
-//     newTag.value.trim() &&
-//     !props.modelValue.tags.includes(newTag.value.trim())
-//   ) {
-//     const updatedData = {
-//       ...props.modelValue,
-//       tags: [...props.modelValue.tags, newTag.value.trim()],
-//     };
-//     emit('update:modelValue', updatedData);
-//     newTag.value = '';	/home/julien/.config/vivaldi/Default
-
-//   }
-// };
-
-// const removeTag = index => {
-//   const updatedTags = [...props.modelValue.tags];
-//   updatedTags.splice(index, 1);
-
-//   const updatedData = {
-//     ...props.modelValue,
-//     tags: updatedTags,
-//   };
-//   emit('update:modelValue', updatedData);
-// };
 </script>
-
-<!-- <style scoped>
-.tag {
-  display: inline-flex;
-  align-items: center;
-  padding: 5px 10px;
-  border-radius: 16px;
-  font-size: 0.9rem;
-}
-
-.tag button {
-  margin-left: 5px;
-  border: none;
-  background: none;
-  cursor: pointer;
-}
-
-.add-tag {
-  display: flex;
-  align-items: center;
-}
-
-.add-tag input {
-  margin-right: 5px;
-  padding: 5px;
-  border: 1px dashed #ccc;
-  border-radius: 16px;
-}
-</style> -->
