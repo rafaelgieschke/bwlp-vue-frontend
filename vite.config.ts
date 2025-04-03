8bd0809277907838950d7aacb7b3db51757a61e7
Julien Rutscho <khodokpro@gmail.com>
2025-04-03 16:25:26 +0200

    oh

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,39 @@ import {fileURLToPath, URL} from 'node:url';
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  base: '/',
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'bwLehrpool',
+        short_name: 'bwLP',
+        description: 'The BWLP website but installed on your machine!',
+        theme_color: '#a599e9',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerSW: {
+        immediate: false,
+      },
+    }),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
